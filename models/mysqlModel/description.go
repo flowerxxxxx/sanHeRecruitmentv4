fdfc0c4238fe121b9aa3da3739c85559ac14946e
YanMingyu <yanmingyu55@gmail.com>
2023-04-22 17:28:58 +0800

    细节修正

--- models/mysqlModel/description.go
+++ models/mysqlModel/description.go
@@ -14,6 +14,6 @@ type Description struct {
 type DescriptionOut struct {
 	Id         int              `json:"id" gorm:"primary_key"`
 	Content    string           `json:"content"`
-	Module     string           `json:"biz"`
+	Module     string           `json:"module"`
 	UpdateTime *timeUtil.MyTime `json:"update_time"`
 }
